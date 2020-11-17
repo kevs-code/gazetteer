b9aff2e25e0be8c02f35fb6f2129bb93045a9355
kevs-code <kevscode@hotmail.com>
2020-11-17 18:31:00 +0000

    cleaner ajax call

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,39 +1,100 @@
-// GOAL KEEP IT WORKING FIRST THEN TIDY!
-const attribution = 'Maps &copy; <a href="http://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
+// GOAL TIDY!
+// CAN I MAKE THIS CLASS USEFUL?
+class MapGet {
+    constructor(tileStyle = 'cycle') {
+      this._dataLink = '<a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
+      this._mapLink = '<a href="http://www.thunderforest.com">Thunderforest</a>';
+      this._attribution = 'Maps &copy; ' +this._mapLink+ ', Data &copy; ' +this._dataLink;
+      this._tileUrl = 'https://tile.thunderforest.com/' + tileStyle + '/{z}/{x}/{y}.png';
+      this._latitude = 54.7023545;
+      this._longitude = -3.2765753;
+      this._ajaxUrl = "libs/php/getCountryCode.php";
+    } 
+    get tileUrl() {
+      return this._tileUrl;
+    }
+    get attribution() {
+      return this._attribution;
+    }
 
-const tileUrl = 'https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png';
-var globalMess;
-var highlight;
-// GB default fields
-let crd = {
-     latitude: 54.7023545,
-     longitude: -3.2765753
-    };
-var mymap = L.map('mapid').setView([crd.latitude, crd.longitude], 4);
-const tiles = L.tileLayer(tileUrl, { attribution });
+    get ajaxUrl() {
+      return this._ajaxUrl;
+    }
+    get crd() {  
+      return {
+          latitude: this._latitude,
+          longitude: this._longitude
+      }
+    }
+}
+
+/*
+cycle
+transport
+Landscape
+outdoors
+transport-dark
+spinal-map
+pioneer
+mobile-atlas
+neighbourhood
+*/
+const myCrd = new MapGet('mobile-atlas');
+const attribution = myCrd.attribution;
+console.log(attribution);
+var mymap = L.map('mapid').setView([myCrd.crd.latitude, myCrd.crd.longitude], 4);
+const tiles = L.tileLayer(myCrd.tileUrl, { attribution });
+
 tiles.addTo(mymap);
-//GLOBALS ABOVE
 
+//globals for reference
+var globalMess;// obj.properties.name sorted countryBorders.geo.json.features obj
+var highlight;// highlight = L.geoJSON(states, { style: myStyle }).addTo(mymap);
+
+
+
+//THIS LOOKS LIKE A CLASS
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, failure);
   } else {
-    getCountryCoords(crd);
     $('#demo').html("Geolocation not supported by this browser.");
+    let data = { lat: myCrd.crd.latitude, lng: myCrd.crd.longitude };
+    testCoords(data);//or just call failure();
   }
 }
 
 function success(pos) {
   let crd = pos.coords;
   $('#demo').html("Latitude: " + crd.latitude + "<br>Longitude: " + crd.longitude);
-  getCountryCoords(crd);
+  let data = { lat: crd.latitude, lng: crd.longitude };
+  testCoords(data);
 }
 
 function failure() {
   $('#demo').html("Latitude: " + crd.latitude + "<br>Longitude: " + crd.longitude);
-  getCountryCoords(crd);
+  let data = { lat: myCrd.crd.latitude, lng: myCrd.crd.longitude };
+  testCoords(data);
 }
 
+
+function testCoords(data) {
+    let args = { url: myCrd.ajaxUrl, type: 'POST', dataType: 'json', data: data };
+    console.log('start');
+    doAjax(args).then(result => {
+        let newcrd = {
+	    latitude: result.geometry.lat,
+	    longitude: result.geometry.lng
+	};
+	initializeMap(newcrd);
+    });
+}
+//needs work to update nationally only!
+function initializeMap(crd) {
+    mymap.setView(new L.LatLng(crd.latitude, crd.longitude), 4);
+}
+
+//SO DOES THIS
 $.getJSON("countryBorders.geo.json", function(geo) {
   let json  = geo.features;
 
@@ -48,93 +109,45 @@ $.getJSON("countryBorders.geo.json", function(geo) {
         }
         return 0;
     });
-   globalMess = geo;
+   globalMess = geo;//could try xom
    populateDropdownFromObjectLiteral('#test', geo);
 });
 
 function populateDropdownFromObjectLiteral(selector, geo) {
     let dropMenu = $(selector);
-    const dropItems = geo.features.map(rest => {
-            let listItem =$('<li/>');
-            let row = $('<a/>');
-            row.attr({
-                // "href": "#",
-                "class": "dropdown-item",
-                "iso_a2": rest.properties.iso_a2,
-		"iso_a3": rest.properties.iso_a3,
-		"iso_n3": rest.properties.iso_n3
+    geo.features.map(rest => {
+        let listItem =$('<li/>');
+        let row = $('<a/>');
+        row.attr({
+            "class": "dropdown-item",
+            "iso_a2": rest.properties.iso_a2,
+		    "iso_a3": rest.properties.iso_a3,
+		    "iso_n3": rest.properties.iso_n3
             });
          row.html(rest.properties.name);
          listItem.append(row);
          dropMenu.append(listItem);
-	 // do something with rest.geometry.coordinates
-    });
-	//console.log(geo.features);
-}
-//suprise
-
-//read result
-
-// end
-
-function getCountryCoords(crd) {
-    let data = { lat: crd.latitude, lng: crd.longitude };
-	console.log('start');
-    getAPI(data).then(result => {
-        let newcrd = {
-	    latitude: result.geometry.lat,
-	    longitude: result.geometry.lng
-	};
-	initializeMap(newcrd);
     });
 }
 
-//needs work to update nationally only!
-function initializeMap(crd) {
-    mymap.setView(new L.LatLng(crd.latitude, crd.longitude), 4);
-}
-//simple promise function needs improve later
-async function getAPI(data) {
-	let result;
-	try {
-		if (!data.iso_a2) {
-		console.log('fire1');
-		// promise 1
-		result = await $.ajax({
-			url: "libs/php/getCountryCode.php",
-			type: 'POST',
-			dataType: 'json',
-			data: data
-		});
-                console.log(result['data'].trim());
-		} else {
-		    result = { data: data.iso_a2 };
-		}
-                // promise 2
-		result2 = await $.ajax({
-			url: "libs/php/getCountryCode.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-			    iso_a2: result['data'].trim()
-			}
-		});
-		console.log(result2);
-		return result2['data'][0];
+async function doAjax(args) {
+    let result;
+    let result2;
+    try {
+        if (!args.data.iso_a2) {
+            result = await $.ajax(args);
+            args.data = { iso_a2: result['data'].trim() }
+        } 
+        
+        result2 = await $.ajax(args);
+        console.log(result2);  
+        return result2['data'][0];
 	} catch (error) {
-		console.log(error);
+		console.log(error);//error.log
 	}
 }
-// end
-
-//main
-$('#test li a').click(function() {
-console.log('hello world');
-console.log($(this).attr("neo"));
-}); //check document against working approach below!
 
 $(document).on('click', '#test li a', function() {
-    //globalName = $(this).attr("iso_a3"); 
     if (highlight) {
     mymap.removeLayer(highlight);
     }
@@ -143,13 +156,9 @@ $(document).on('click', '#test li a', function() {
     console.log($(this).attr("iso_a2"));
     let data = { iso_a2: $(this).attr("iso_a2") };
     testCoords(data);
-    
-   //test
-   
-    const dropItems = globalMess.features.map(rest => {
+   //test 
+   const dropItems = globalMess.features.map(rest => {//is dropItems live still instead
 	if(rest.properties.iso_a2 == $(this).attr("iso_a2")) {
-	 // do something with rest.geometry.coordinates
-         // console.log(rest.geometry.coordinates);
         let states = {
             "type": "Feature",
             "properties": rest.properties,
@@ -165,26 +174,6 @@ function updateMap(states) {
 var myStyle = { "color": "#0000FF" };
 highlight = L.geoJSON(states, { style: myStyle }).addTo(mymap);
 }
-/*
-function updateMap(crd) {
-    mymap.setView(new L.LatLng(crd.latitude, crd.longitude), 4);
-}
-*/
-
-//  map.panTo(new L.LatLng(lat, lng));
-//  map.setView(new L.LatLng(lat, lng), 8);
-function testCoords(data) {
-    console.log('start');
-    getAPI(data).then(result => {
-        let newcrd = {
-	    latitude: result.geometry.lat,
-	    longitude: result.geometry.lng
-	};
-	initializeMap(newcrd);
-    });
-}
-
-
 
 // recommended approach to document ready!
 $(function () {
